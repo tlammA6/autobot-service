572814167a1a687a35e1f0a1e4ebaa0615e87d91
tlammA6 <theron.lamm@binarystar.biz>
2021-05-05 00:05:30 -0400

    created dynamic repository

---
+++ src/service/vehicle-service.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { VehicleServiceEntity } from '../entity/vehicle-service.entity';
+import { VehicleServiceRepository } from '../repository/vehicle-service.repository';
+
+@Injectable()
+export class VehicleServiceService {
+  constructor(
+    private readonly vehicleServiceRepository: VehicleServiceRepository,
+  ) {}
+
+  public async create(
+    vehicleServiceEntity: VehicleServiceEntity,
+  ): Promise<VehicleServiceEntity> {
+    return await this.vehicleServiceRepository.create(vehicleServiceEntity);
+  }
+
+  public async findAll(
+    vehicleServiceEntity: VehicleServiceEntity,
+  ): Promise<VehicleServiceEntity[]> {
+    return this.vehicleServiceRepository.findAll(vehicleServiceEntity);
+  }
+
+  public async find(
+    vehicleServiceEntity: VehicleServiceEntity,
+  ): Promise<VehicleServiceEntity> {
+    return this.vehicleServiceRepository.findOne(vehicleServiceEntity);
+  }
+
+  async update(
+    vehicleServiceEntity: VehicleServiceEntity,
+  ): Promise<VehicleServiceEntity> {
+    return await this.vehicleServiceRepository.update(vehicleServiceEntity);
+  }
+
+  async delete(vehicleServiceEntity: VehicleServiceEntity): Promise<void> {
+    return await this.vehicleServiceRepository.delete(vehicleServiceEntity);
+  }
+}
