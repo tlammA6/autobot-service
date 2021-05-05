36ba7a453662a35aa9625b4efa92eb17e87c479e
tlammA6 <theron.lamm@binarystar.biz>
2021-05-05 14:11:34 -0400

    added lex and refactored repo

--- src/repository/customer-vehicle.repository.ts
+++ src/repository/customer-vehicle.repository.ts
@@ -4,7 +4,17 @@ import { BaseRepository } from './base.repository';
 
 @Injectable()
 export class CustomerVehicleRepository extends BaseRepository<CustomerVehicleEntity> {
-  createEntity(): CustomerVehicleEntity {
+  // This is needed in order for the BaseRepository to create a new entity of this Type.
+  createType(): CustomerVehicleEntity {
     return new CustomerVehicleEntity();
+  }
+
+  /** This was needed to allow both Entity interface & Generic Type to both work in the base.repository.
+   *  Will look into better solution
+   * */
+  convertToEntity(
+    customerVehicleEntity: CustomerVehicleEntity,
+  ): CustomerVehicleEntity {
+    return customerVehicleEntity;
   }
 }
